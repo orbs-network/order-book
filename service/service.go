301fcc29e1443f986718ff358ca136d39957b5ea
Luke Rogerson <luke@orbs.com>
2023-10-17 10:34:22 +0100

    Add test for find_order_by_id

--- service/service.go
+++ service/service.go
@@ -14,7 +14,6 @@ import (
 type OrderBookStore interface {
 	StoreOrder(ctx context.Context, order models.Order) error
 	RemoveOrder(ctx context.Context, orderId uuid.UUID) error
-	FindOrder(ctx context.Context, input models.FindOrderInput) (*models.Order, error)
 	FindOrderById(ctx context.Context, orderId uuid.UUID) (*models.Order, error)
 	GetOrdersAtPrice(ctx context.Context, symbol models.Symbol, price decimal.Decimal) ([]models.Order, error)
 	GetBestPriceFor(ctx context.Context, symbol models.Symbol, side models.Side) (models.Order, error)
