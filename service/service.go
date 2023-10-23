8e20f9600d12212e314ec234a2bcdc2c1c12091a
uvorbs <yuval@orbs.com>
2023-10-23 14:31:24 +0300

    lh side

--- service/service.go
+++ service/service.go
@@ -12,13 +12,15 @@ import (
 )
 
 type OrderBookStore interface {
+	// MM side
 	StoreOrder(ctx context.Context, order models.Order) error
-	StoreAuction(ctx context.Context, auctionID string, fillOrders map[string]string) error
 	RemoveOrder(ctx context.Context, order models.Order) error
 	FindOrderById(ctx context.Context, orderId uuid.UUID) (*models.Order, error)
 	GetOrdersAtPrice(ctx context.Context, symbol models.Symbol, price decimal.Decimal) ([]models.Order, error)
 	GetBestPriceFor(ctx context.Context, symbol models.Symbol, side models.Side) (models.Order, error)
 	GetMarketDepth(ctx context.Context, symbol models.Symbol, depth int) (models.MarketDepth, error)
+	// LH side
+	StoreAuction(ctx context.Context, auctionID string, fillOrders map[string]string) error
 	GetMinAsk(ctx context.Context, symbol models.Symbol) OrderIter
 	GetMaxBid(ctx context.Context, symbol models.Symbol) OrderIter
 }
