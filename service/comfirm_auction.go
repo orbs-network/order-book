f0f4efc43bef3be1da9dd7139eec716d53f733ba
uvorbs <yuval@orbs.com>
2023-10-24 17:04:46 +0300

    before merge main

---
+++ service/comfirm_auction.go
@@ -0,0 +1,11 @@
+package service
+
+import (
+	"context"
+)
+
+// orderID->amount bought or sold in A token always
+
+func (s *Service) confirmAuction(ctx context.Context, auctionId string) error {
+	return nil
+}
