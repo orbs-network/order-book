19a759208195575f93e5be965a5ca291f735f61b
uvorbs <yuval@orbs.com>
2023-10-26 10:31:54 +0300

    tmp

--- service/auction.go
+++ service/auction.go
@@ -2,6 +2,9 @@ package service
 
 import (
 	"context"
+
+	"github.com/orbs-network/order-book/models"
+	"github.com/orbs-network/order-book/utils/logger/logctx"
 )
 
 // orderID->amount bought or sold in A token always
@@ -10,14 +13,45 @@ type ConfirmAuctionRes struct {
 	Orders []string
 }
 
+func (s *Service) validateFillOrder(fillOrder string) *models.Order {
+	// get order from ID
+
+	// check if order is still open
+	// order.size - filledOrder.filled >= fillOrder.amount
+	return nil
+}
+
 func (s *Service) ConfirmAuction(ctx context.Context, auctionId string) (ConfirmAuctionRes, error) {
 	// get auction from store
+	arrOfFillOrders := s.orderBookStore.GetAuction(auctionId)
 
 	// validate all orders of auction
+	for fillOrder := range arrOfFillOrders {
+		// get order by ID
+		// validate order
+		// if order is valid
+		//order := s.validateFillOrder(models.Order{})
+
+		// validate
+		// if order == nil {
+		// 	logctx.Warn(ctx, models.ErrInsufficientLiquity.Error())
+		// 	return ConfirmAuctionRes{}, models.ErrInsufficientLiquity
+		// }
+
+	}
 
 	// return orders signatures
 
 	// add oredebook signature on the buffer
+
+	// lock funds
+
+	// remove auction store
+	//s.orderBookStore.RemoveAuction(auctionId)
+
+	// error
+	logctx.Warn(ctx, models.ErrInsufficientLiquity.Error())
+	return ConfirmAuctionRes{}, models.ErrInsufficientLiquity
 
 	return ConfirmAuctionRes{}, nil
 }
