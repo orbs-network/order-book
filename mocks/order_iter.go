f29bf2be85089ab5af4c7806def252b05155b149
uvorbs <yuval@orbs.com>
2023-11-05 17:56:09 +0200

    implement iter mock

--- mocks/order_iter.go
+++ mocks/order_iter.go
@@ -4,18 +4,31 @@ import (
 	"context"
 
 	"github.com/orbs-network/order-book/models"
+	"github.com/orbs-network/order-book/utils/logger/logctx"
 )
 
-type OrderIter struct {
-	Error          error
-	Order          *models.Order
-	ShouldHaveNext bool
+type OrderIterMock struct {
+	orders []models.Order
+	index  int
+	// Error          error
+	// Order          *models.Order
+	// ShouldHaveNext bool
 }
 
-func (o *OrderIter) HasNext() bool {
-	return o.ShouldHaveNext
+func (i OrderIterMock) HasNext() bool {
+	return i.index < (len(i.orders) - 1)
+
 }
 
-func (o *OrderIter) Next(ctx context.Context) *models.Order {
-	return o.Order
+func (i OrderIterMock) Next(ctx context.Context) *models.Order {
+	if i.index >= len(i.orders) {
+		logctx.Error(ctx, "Error iterator reached last element")
+		return nil
+	}
+
+	// increment index
+	i.index = i.index + 1
+	// get order
+
+	return &i.orders[i.index]
 }
