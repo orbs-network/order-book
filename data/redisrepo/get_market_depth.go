82d5d2b3d32de53fd3b96c1a7d70f5c54d5cfde3
Luke Rogerson <contact@lukerogerson.me>
2023-12-10 12:23:05 +0000

    Use GetOrdersByIds method for market depth (#61)

--- data/redisrepo/get_market_depth.go
+++ data/redisrepo/get_market_depth.go
@@ -2,6 +2,8 @@ package redisrepo
 
 import (
 	"context"
+	"fmt"
+	"sync"
 	"time"
 
 	"github.com/google/uuid"
@@ -20,66 +22,95 @@ func (r *redisRepository) GetMarketDepth(ctx context.Context, symbol models.Symb
 		Time:   time.Now().Unix(),
 	}
 
-	errChan := make(chan error, 2)
+	// Fetch sell and buy side orders concurrently
+	var askOrderIds, bidOrderIds []uuid.UUID
+	var asksErr, bidsErr error
+	var wg sync.WaitGroup
+	wg.Add(2)
 
-	// Asks
-	sellSideKey := CreateSellSidePricesKey(symbol)
+	// Fetch Asks
 	go func() {
-		asks, err := r.client.ZRange(ctx, sellSideKey, 0, int64(depth-1)).Result()
-		if err != nil {
-			logctx.Error(ctx, "Error fetching asks", logger.Error(err))
-			errChan <- err
-			return
-		}
-
-		for _, orderIDStr := range asks {
-			orderId, err := uuid.Parse(orderIDStr)
-			if err != nil {
-				logctx.Error(ctx, "Error parsing ask order id", logger.Error(err))
-				continue
-			}
-			order, err := r.FindOrderById(ctx, orderId, false)
-			if err != nil {
-				logctx.Error(ctx, "Error fetching order", logger.Error(err))
-				continue
-			}
-			marketDepth.Asks = append(marketDepth.Asks, []decimal.Decimal{order.Price, order.GetAvailableSize()})
-		}
-		errChan <- nil
+		defer wg.Done()
+		askOrderIds, asksErr = r.fetchOrderIds(ctx, CreateSellSidePricesKey(symbol), depth)
 	}()
 
-	// Bids
-	buySideKey := CreateBuySidePricesKey(symbol)
+	// Fetch Bids
 	go func() {
-		bids, err := r.client.ZRevRange(ctx, buySideKey, 0, int64(depth-1)).Result()
-		if err != nil {
-			logctx.Error(ctx, "Error fetching bids", logger.Error(err))
-			errChan <- err
-			return
-		}
-
-		for _, orderIDStr := range bids {
-			orderId, err := uuid.Parse(orderIDStr)
-			if err != nil {
-				logctx.Error(ctx, "Error parsing bid order id", logger.Error(err))
-				continue
-			}
-			order, err := r.FindOrderById(ctx, orderId, false)
-			if err != nil {
-				logctx.Error(ctx, "Error fetching order", logger.Error(err))
-				continue
-			}
-			marketDepth.Bids = append(marketDepth.Bids, []decimal.Decimal{order.Price, order.GetAvailableSize()})
-		}
-		errChan <- nil
+		defer wg.Done()
+		bidOrderIds, bidsErr = r.fetchOrderIds(ctx, CreateBuySidePricesKey(symbol), depth)
 	}()
 
-	// Wait for both goroutines to finish
-	for i := 0; i < 2; i++ {
-		if err := <-errChan; err != nil {
-			return marketDepth, err
+	wg.Wait()
+	if asksErr != nil {
+		logctx.Error(ctx, "failed to fetch asks", logger.Error(asksErr))
+		return marketDepth, fmt.Errorf("failed to fetch asks: %v", asksErr)
+	}
+	if bidsErr != nil {
+		logctx.Error(ctx, "failed to fetch bids", logger.Error(bidsErr))
+		return marketDepth, fmt.Errorf("failed to fetch bids: %v", bidsErr)
+	}
+
+	// Concatenate ask and bid order IDs
+	allOrderIds := append(askOrderIds, bidOrderIds...)
+
+	// Fetch all orders in bulk
+	var orders []models.Order
+	var err error
+	for i := 0; i < len(allOrderIds); i += MAX_ORDER_IDS {
+		end := i + MAX_ORDER_IDS
+		if end > len(allOrderIds) {
+			end = len(allOrderIds)
+		}
+
+		orders, err = r.FindOrdersByIds(ctx, allOrderIds[i:end])
+		if err != nil {
+			logctx.Error(ctx, "failed to find orders by IDs", logger.Error(err))
+			return marketDepth, fmt.Errorf("failed to find orders by IDs: %v", err)
 		}
 	}
 
+	// Map orders by their ID for quick lookup
+	orderMap := make(map[uuid.UUID]models.Order)
+	for _, order := range orders {
+		orderMap[order.Id] = order
+	}
+
+	// Process asks
+	for _, orderId := range askOrderIds {
+		if order, ok := orderMap[orderId]; ok {
+			marketDepth.Asks = append(marketDepth.Asks, []decimal.Decimal{order.Price, order.GetAvailableSize()})
+		}
+	}
+
+	// Process bids
+	for _, orderId := range bidOrderIds {
+		if order, ok := orderMap[orderId]; ok {
+			marketDepth.Bids = append(marketDepth.Bids, []decimal.Decimal{order.Price, order.GetAvailableSize()})
+		}
+	}
+
+	logctx.Info(ctx, "fetched market depth", logger.String("symbol", symbol.String()), logger.Int("depth", depth), logger.Int("numAsks", len(marketDepth.Asks)), logger.Int("numBids", len(marketDepth.Bids)))
 	return marketDepth, nil
+}
+
+// fetchOrderIds fetches order IDs for a given key and depth
+func (r *redisRepository) fetchOrderIds(ctx context.Context, key string, depth int) ([]uuid.UUID, error) {
+	orderStrIds, err := r.client.ZRange(ctx, key, 0, int64(depth-1)).Result()
+	if err != nil {
+		logctx.Error(ctx, "failed to fetch order IDs from Redis", logger.Error(err), logger.String("key", key), logger.Int("depth", depth))
+		return nil, fmt.Errorf("failed to fetch order IDs from Redis: %v", err)
+	}
+
+	orderIds := make([]uuid.UUID, len(orderStrIds))
+	for i, strId := range orderStrIds {
+		orderId, err := uuid.Parse(strId)
+		if err != nil {
+			logctx.Error(ctx, "failed to parse order ID", logger.Error(err), logger.String("key", key), logger.Int("depth", depth), logger.String("strId", strId))
+			return nil, fmt.Errorf("failed to parse order ID: %v", err)
+		}
+		orderIds[i] = orderId
+	}
+
+	logctx.Info(ctx, "fetched order IDs", logger.String("key", key), logger.Int("depth", depth), logger.Int("numOrders", len(orderIds)))
+	return orderIds, nil
 }
