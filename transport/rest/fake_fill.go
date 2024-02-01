08b5dc0ac482c5d1dc26e7ee0742fdc8eaafa957
uvorbs <yuval@orbs.com>
2024-02-01 13:11:42 +0200

    fix double res write

--- transport/rest/fake_fill.go
+++ transport/rest/fake_fill.go
@@ -9,18 +9,17 @@ import (
 	"github.com/orbs-network/order-book/utils/logger/logctx"
 )
 
-type FakeFillRes struct {
-	OrderId []string `json:"orderId"`
-}
-
 func (h *Handler) fakeFill(w http.ResponseWriter, r *http.Request) {
 	res := h.handleQuote(w, r, true)
-	logctx.Info(r.Context(), "FakeFill", logger.String("swapId", res.SwapId), logger.String("InAmount", res.InAmount), logger.String("OutAmount", res.OutAmount))
+	if res == nil {
+		logctx.Warn(r.Context(), "FakeFill Failed")
+		return
+	}
+	logctx.Debug(r.Context(), "FakeFill", logger.String("swapId", res.SwapId), logger.String("InAmount", res.InAmount), logger.String("OutAmount", res.OutAmount))
 	err := h.svc.FillSwap(r.Context(), uuid.MustParse(res.SwapId))
 	if err != nil {
 		restutils.WriteJSONError(r.Context(), w, http.StatusBadRequest, err.Error())
 		return
 	}
-
-	restutils.WriteJSONResponse(r.Context(), w, http.StatusOK, res, logger.String("Swap Fake Fill", res.SwapId))
+	// response is written in FillSwap
 }
