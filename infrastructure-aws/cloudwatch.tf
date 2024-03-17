f0e6c41c8e0d6d3651b8cace16f95f07c5892470
Luke Rogerson <contact@lukerogerson.me>
2024-03-17 21:02:59 +0000

    Remove AWS references (#154)

--- infrastructure-aws/cloudwatch.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_cloudwatch_log_group" "log_group" {
-  name              = "order-book-${var.environment_name}"
-  retention_in_days = 7
-  tags              = local.tags
-}
--- infrastructure-aws/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "app_url" {
-  value       = aws_apprunner_service.order-book.service_url
-  description = "The URL of the deployed application"
-}
