66b64cedc4173180aa8d8ad7b6a99cb719745026
uvorbs <yuval@orbs.com>
2024-03-19 13:53:27 +0200

    pr review fix

--- service/cancel_orders_for_user.go
+++ service/cancel_orders_for_user.go
@@ -16,25 +16,25 @@ func (s *Service) CancelOrdersForUser(ctx context.Context, userId uuid.UUID, sym
 		logctx.Info(ctx, "no orders found for user", logger.String("userId", userId.String()))
 		return []uuid.UUID{}, err
 	}
+
+	orders, err := s.orderBookStore.FindOrdersByIds(ctx, ids, true)
+	if err != nil {
+		logctx.Error(ctx, "FindOrdersByIds open orders failed", logger.Error(err), logger.String("userId", userId.String()))
+		return []uuid.UUID{}, err
+	}
 	res := []uuid.UUID{}
-	for _, id := range ids {
-		order, err := s.getOrder(ctx, false, id)
-		if err != nil {
-			logctx.Error(ctx, "could not get order", logger.Error(err), logger.String("orderId", id.String()))
-		}
-		if order != nil {
-			// matching symbol only if provided
-			if symbol == "" || order.Symbol == symbol {
-				uid, err := s.CancelOrder(ctx, CancelOrderInput{
-					Id:          id,
-					IsClientOId: false,
-					UserId:      userId,
-				}) //, symbol)
-				if err != nil {
-					logctx.Error(ctx, "could not cancel order", logger.Error(err), logger.String("orderId", uid.String()))
-				}
-				res = append(res, *uid)
+	for _, order := range orders {
+		// matching symbol only if provided
+		if symbol == "" || order.Symbol == symbol {
+			uid, err := s.CancelOrder(ctx, CancelOrderInput{
+				Id:          order.Id,
+				IsClientOId: false,
+				UserId:      userId,
+			})
+			if err != nil {
+				logctx.Error(ctx, "could not cancel order", logger.Error(err), logger.String("orderId", uid.String()))
 			}
+			res = append(res, *uid)
 		}
 	}
 
