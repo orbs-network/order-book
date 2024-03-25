d27d416ac626f22f8c991936b2fd698e74607146
uvorbs <yuval@orbs.com>
2024-03-25 11:01:27 +0200

    code clean

--- transport/rest/get_orders_for_user.go
+++ transport/rest/get_orders_for_user.go
@@ -72,7 +72,7 @@ func (h *Handler) GetFilledOrdersForUser(w http.ResponseWriter, r *http.Request)
 
 	jsonData, err := json.Marshal(res)
 	if err != nil {
-		logctx.Error(r.Context(), "failed to marshal response", logger.Error(err), logger.String("orderId", user.Id.String()))
+		logctx.Error(r.Context(), "failed to marshal response", logger.Error(err), logger.String("userId", user.Id.String()))
 		restutils.WriteJSONError(ctx, w, http.StatusInternalServerError, "Error getting orders. Try again later")
 		return
 	}
@@ -80,8 +80,43 @@ func (h *Handler) GetFilledOrdersForUser(w http.ResponseWriter, r *http.Request)
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	if _, err := w.Write(jsonData); err != nil {
-		logctx.Error(r.Context(), "failed to write response", logger.Error(err), logger.String("orderId", user.Id.String()))
+		logctx.Error(r.Context(), "failed to write response", logger.Error(err), logger.String("userId", user.Id.String()))
 		restutils.WriteJSONError(ctx, w, http.StatusInternalServerError, "Error getting orders. Try again later")
 	}
 
+}
+
+func (h *Handler) GetSwapFills(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	user := utils.GetUserCtx(ctx)
+	if user == nil {
+		logctx.Error(ctx, "user should be in context")
+		restutils.WriteJSONError(ctx, w, http.StatusUnauthorized, "User not found")
+		return
+	}
+
+	logctx.Debug(r.Context(), "user trying to get their filled orders", logger.String("userId", user.Id.String()))
+
+	symbol := models.Symbol("MATIC-USDC")
+	startAt, endAt := getStartEndTime(r)
+	fills, err := h.svc.GetSwapFills(r.Context(), user.Id, symbol, startAt, endAt)
+	if err != nil {
+		logctx.Error(r.Context(), "failed GetSwapFills", logger.Error(err), logger.String("userId", user.Id.String()))
+		if err == models.ErrMaxRecExceeded {
+			// narrow down the time range, 256 exceeded
+			restutils.WriteJSONError(ctx, w, http.StatusRequestEntityTooLarge, err.Error())
+		} else {
+			restutils.WriteJSONError(ctx, w, http.StatusInternalServerError, "Error getting swaps. Try again later")
+		}
+	}
+	jsonData, err := json.Marshal(fills)
+	if err != nil {
+		logctx.Error(r.Context(), "failed to Marshal orders", logger.Error(err), logger.String("userId", user.Id.String()))
+		restutils.WriteJSONError(ctx, w, http.StatusInternalServerError, "Error Marshalling swap orders.")
+	}
+
+	if _, err := w.Write(jsonData); err != nil {
+		logctx.Error(r.Context(), "failed to write response", logger.Error(err), logger.String("orderId", user.Id.String()))
+		restutils.WriteJSONError(ctx, w, http.StatusInternalServerError, "Error write response.")
+	}
 }
