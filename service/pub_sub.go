97156823ce282040716cc3f09dba2fd450ee38ca
uvorbs <yuval@orbs.com>
2024-03-26 16:31:31 +0200

    make publish available both from evm proc and server proc

--- service/pub_sub.go
+++ service/pub_sub.go
@@ -2,9 +2,11 @@ package service
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 
 	"github.com/google/uuid"
+	"github.com/orbs-network/order-book/data/store"
 	"github.com/orbs-network/order-book/models"
 	"github.com/orbs-network/order-book/utils/logger"
 	"github.com/orbs-network/order-book/utils/logger/logctx"
@@ -24,30 +26,61 @@ func (s *Service) SubscribeUserOrders(ctx context.Context, userId uuid.UUID) (ch
 	return channel, nil
 }
 
-func (s *EvmClient) publishOrderEvent(ctx context.Context, order *models.Order) {
+func publishFillEvent(ctx context.Context, store store.OrderBookStore, userId uuid.UUID, fill models.Fill) {
+	//value, err := json.Marshal(fill)
+	value, err := json.Marshal(struct {
+		Event string `json:"event"`
+		models.Fill
+	}{
+		Event: "order-fill",
+		Fill:  fill,
+	})
+	if err != nil {
+		logctx.Error(ctx, "failed to marshal order to json", logger.Error(err))
+	}
+
+	key := models.CreateUserOrdersEventKey(userId)
+	if err := store.PublishEvent(ctx, key, value); err != nil {
+		logctx.Error(ctx, "failed to publish fill event", logger.String("event", key), logger.Error(err))
+	}
+
+}
+func (e *EvmClient) publishFillEvent(ctx context.Context, userId uuid.UUID, fill models.Fill) {
+	publishFillEvent(ctx, e.orderBookStore, userId, fill)
+}
+
+func (s *Service) publishFillEvent(ctx context.Context, userId uuid.UUID, fill models.Fill) {
+	publishFillEvent(ctx, s.orderBookStore, userId, fill)
+}
+
+func publishOrderEvent(ctx context.Context, store store.OrderBookStore, order *models.Order) {
 	key, value, err := createOrderEvent(ctx, order)
 	if err != nil {
 		return
 	}
 
-	if err := s.orderBookStore.PublishEvent(ctx, key, value); err != nil {
+	if err := store.PublishEvent(ctx, key, value); err != nil {
 		logctx.Error(ctx, "failed to publish order event", logger.String("event", key), logger.Error(err))
 	}
 }
 
+func (e *EvmClient) publishOrderEvent(ctx context.Context, order *models.Order) {
+	publishOrderEvent(ctx, e.orderBookStore, order)
+}
 func (s *Service) publishOrderEvent(ctx context.Context, order *models.Order) {
-	key, value, err := createOrderEvent(ctx, order)
-	if err != nil {
-		return
-	}
-
-	if err := s.orderBookStore.PublishEvent(ctx, key, value); err != nil {
-		logctx.Error(ctx, "failed to publish order event", logger.String("event", key), logger.Error(err))
-	}
+	publishOrderEvent(ctx, s.orderBookStore, order)
 }
 
 func createOrderEvent(ctx context.Context, order *models.Order) (key string, value []byte, err error) {
-	value, err = order.ToJson()
+	//value, err = order.ToJson()
+
+	value, err = json.Marshal(struct {
+		Event string `json:"event"`
+		models.Order
+	}{
+		Event: "order-changed",
+		Order: *order,
+	})
 	if err != nil {
 		logctx.Error(ctx, "failed to marshal order to json", logger.Error(err))
 	}
